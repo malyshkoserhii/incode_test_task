8dbf2dd76a0ce81f9dfbca1a468bd4244a0c5fb1
Serhii Malyshko <malyshkosergiy@gmail.com>
2024-03-12 16:20:39 +0200

    Added basic setup configs

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,18 @@
 module.exports = {
   root: true,
-  extends: '@react-native',
+  parser: '@babel/eslint-parser',
+  env: {
+    'react-native/react-native': true,
+  },
+  extends: ['plugin:react/recommended', 'plugin:react-native/all', 'prettier'],
+  ignorePatterns: ['.eslintrc.js'],
+  plugins: ['prettier', 'react-hooks', 'react', 'react-native'],
+  rules: {
+    'no-console': 'error',
+    'max-len': ['error', 120],
+    'ordered-imports': 0,
+    'no-unused-expressions': [2, {allowShortCircuit: true, allowTernary: true}],
+    indent: ['error', 'tab'],
+    quotes: ['error', 'single'],
+  },
 };
---
+++ src/types/env.d.ts
@@ -0,0 +1,4 @@
+declare module '@env' {
+	export const FIREBASE_WEB_CLIENT_ID: string;
+	export const FIREBASE_IOS_CLIENT_ID: string;
+}
